0aedfb1c645acacef74b0b6ba492055a8dbd2ab4
ChampBlue <oliekim11@gmail.com>
2024-06-29 20:16:21 +0900

    up

--- 1_ROS 기초 이해/2_ros구성요소/1_노드/2_frist_node.py
+++ 1_ROS 기초 이해/2_ros구성요소/1_노드/2_frist_node.py
@@ -3,12 +3,13 @@ from std_msgs.msg import String
 
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10) 
-    rospy.init_node('talker', anonymous=True) 
-    rate = rospy.Rate(10)       
+    rospy.init_node('talker') 
+    rate = rospy.Rate(1)       
     
     for i in range(5):
-        hello_str = f"hello world{rospy.get_time()}"
-        rospy.loginfo(hello_str)
+        hello_str = f"hello world  {rospy.get_time()}"
+        #rospy.loginfo(hello_str)
+        print(hello_str)
         pub.publish(hello_str)
         rate.sleep()    
     
